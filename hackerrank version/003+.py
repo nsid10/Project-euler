0b85f0e066d68a815f4587593c5034478b18fba3
Nahian Siddique <nsid10@hotmail.com>
2018-06-27 07:05:55 +0000

    Add new file

---
+++ hackerrank version/003+.py
@@ -0,0 +1,27 @@
+def primefactor(n):
+    if type(n) != int:
+        raise TypeError("argument must be of type 'int'")
+    if n < 2:
+        return []
+    factors = []
+    while n % 2 == 0:
+        n /= 2
+        factors.append(2)
+    x = 3
+    limit = int(n**0.5 + 1)
+    while x <= limit:
+        if n % x == 0:
+            n //= x
+            limit = int(n**0.5 + 1)
+            factors.append(x)
+            continue
+        x += 2
+    if n != 1:
+        factors.append(int(n))
+    return factors
+
+
+t = int(input().strip())
+for i in range(t):
+    n = int(input().strip())
+    print(primefactor(n)[-1])
