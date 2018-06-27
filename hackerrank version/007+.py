41318c86039da1c0298f3fec2d5391181e164de1
Nahian Siddique <nsid10@hotmail.com>
2018-06-27 08:41:07 +0000

    Add new file

---
+++ hackerrank version/007+.py
@@ -0,0 +1,20 @@
+def prime_sieve(n):
+    if n < 3:
+        return []
+    numbers = [True] * (n // 2)
+    for i in range(3, int(n**0.5 + 1), 2):
+        if numbers[(i - 1) // 2]:
+            for j in range(i * i, n, 2 * i):
+                numbers[(j - 1) // 2] = False
+    sieve = [2]
+    for i in range(1, n // 2):
+        if numbers[i]:
+            sieve.append(2 * i + 1)
+    return sieve
+
+
+sieve = prime_sieve(125000)
+t = int(input().strip())
+for x in range(t):
+    n = int(input().strip())
+    print(sieve[n - 1])
