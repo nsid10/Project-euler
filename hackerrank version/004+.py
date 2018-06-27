80ab21ea4db8ad7a97b1497ba7e8bca924d1b981
Nahian Siddique <nsid10@hotmail.com>
2018-06-27 07:54:30 +0000

    Update 004+.py

--- hackerrank version/004+.py
+++ hackerrank version/004+.py
@@ -8,12 +8,11 @@ def palindrome(n):
 t = int(input().strip())
 for x in range(t):
     target = int(input().strip())
-    dg = 3
     sign = False
-    for i in range(target - 1, (10**(dg - 1))**2, -1):
+    for i in range((10**3 - 1)**2, (10**2)**2, -1):
         if palindrome(i):
-            for k in range(10**dg - 1, 10**(dg - 1), -1):
-                if i % k == 0 and i / k >= 10**(dg - 1) and i / k < 10**dg:
+            for k in range(10**3 - 1, 10**2, -1):
+                if i % k == 0 and i / k >= 10**2 and i / k < 10**3:
                     sign = True
                     break
         if sign:
