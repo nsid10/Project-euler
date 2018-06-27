2fca2c0be4c4756140da9ba66038561e4f6febe6
Nahian Siddique <nsid10@hotmail.com>
2018-06-27 07:52:26 +0000

    Update 005.py

--- problems/005.py
+++ problems/005.py
@@ -5,8 +5,8 @@ def lcm(box):
     h = box[0]
     for i in range(1, len(box)):
         div = math.gcd(h, box[i])
-        h = int((h / div) * (box[i] / div) * div)
-    print(h)
+        h *= box[i] // div
+    return h
 
 
 lcm(list(range(1, 20 + 1)))
