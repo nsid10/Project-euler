8b2a84abbd0937131b55ac2d435f650af7ad847d
Nahian Siddique <nsid10@hotmail.com>
2018-06-28 10:16:39 +0000

    Update 017.py

--- problems/017.py
+++ problems/017.py
@@ -1,27 +1,30 @@
 def cardinal(num):
-    if num >= 0 and num < 20:
-        names = ["zero", "one", "two", "three", "four", "five", "six", "seven",
-                 "eight", "nine", "ten", "eleven", "twelve", "thirteen",
-                 "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
-                 "nineteen"]
+    if num < 20:
+        names = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
+            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        ]
         return names[num]
-    if num > 19 and num < 100:
+    if num < 100:
         if num % 10 == 0:
-            tens = ["twenty", "thirty", "forty", "fifty",
-                    "sixty", "seventy", "eighty", "ninety"]
-            return tens[int(num / 10) - 2]
-        return cardinal((num // 10) * 10) + '-' + cardinal(num % 10)
-    if num > 99 and num < 1000:
+            tens = [
+                "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+                "eighty", "ninety"
+            ]
+            return tens[int(num // 10) - 2]
+        return cardinal((num // 10) * 10) + " " + cardinal(num % 10)
+    if num < 1000:
         if num % 100 == 0:
-            return cardinal(num // 100) + ' hundred'
-        return cardinal(num // 100) + ' hundred and ' + cardinal(num % 100)
-    if num > 999 and num < 1000000:
+            return cardinal(num // 100) + " hundred"
+        return cardinal(num // 100) + " hundred and " + cardinal(num % 100)
+    if num < 1000000:
         if num % 1000 == 0:
-            return cardinal(num // 1000) + ' thousand'
-        return cardinal(num // 1000) + ' thousand and ' + cardinal(num % 1000)
+            return cardinal(num // 1000) + " thousand"
+        return cardinal(num // 1000) + " thousand and " + cardinal(num % 1000)
 
 
 letters = 0
 for i in range(1, 1000 + 1):
-    letters += len(cardinal(i)) - cardinal(i).count(' ')
+    letters += len(cardinal(i)) - cardinal(i).count(" ")
 print(letters)
