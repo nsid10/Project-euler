a05bd66d15490fa496bdb1c6f9aabfc0762eb461
Nahian Siddique <nsid10@hotmail.com>
2018-07-01 10:18:17 +0000

    Update 030.py

--- problems/030.py
+++ problems/030.py
@@ -1,13 +1,5 @@
-def fifth(n):
-    n = str(n)
-    sum = 0
-    for i in n:
-        sum += (int(i))**5
-    return sum
-
-
-sum = 0
+total = 0
 for i in range(10, 1000000):
-    if fifth(i) == i:
-        sum += i
-print(sum)
+    if sum([(int(n))**5 for n in str(i)]) == i:
+        total += i
+print(total)
