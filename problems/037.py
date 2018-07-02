c9041d18309875cb261c4bd078700baae3bac189
Nahian Siddique <nsid10@hotmail.com>
2018-07-02 09:58:38 +0000

    Update 037.py

--- problems/037.py
+++ problems/037.py
@@ -1,19 +1,31 @@
 def primality(n):
     if type(n) != int:
         raise TypeError("argument must be of type 'int'")
-    if n < 2:
-        return False
     if n == 2:
         return True
-    if n % 2 == 0:
+    if n < 2 or n % 2 == 0:
         return False
-    for x in range(3, int(n**0.5 + 1), 2):
-        if n % x == 0:
+    for i in range(3, int(n**0.5 + 1), 2):
+        if n % i == 0:
             return False
     return True
 
 
+def prime_sieve(n):
+    if type(n) != int:
+        raise TypeError("argument must be of type 'int'")
+    if n < 3:
+        return []
+    number = [False] + [True] * (n // 2 - 1)
+    for i in range(3, int(n**0.5 + 1), 2):
+        if number[(i - 1) // 2]:
+            number[i**2 // 2::i] = [False] * ((n - 1) // i - (n - 1) // (2 * i) - (i - 1) // 2)
+    return [2] + [2 * i + 1 for i in range(n // 2) if number[i]]
+
+
 def trunc(n):
+    if n < 10:
+        return False
     n = str(n)
     for i in range(1, len(n)):
         if not primality(int(n[i:])):
@@ -24,9 +36,9 @@ def trunc(n):
     return True
 
 
+N = 1000000
 sum = 0
-for i in range(10, 1000000):
-    if primality(i):
-        if trunc(i):
-            sum += i
+for i in prime_sieve(N):
+    if trunc(i):
+        sum += i
 print(sum)
