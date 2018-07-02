f8fa49a1fe777b5c4c8ecbe5b9710511e4adae98
Nahian Siddique <nsid10@hotmail.com>
2018-07-02 09:30:24 +0000

    Update 035.py

--- problems/035.py
+++ problems/035.py
@@ -16,17 +16,29 @@ def primality(n):
     return True
 
 
+def prime_sieve(n):
+    if type(n) != int:
+        raise TypeError("argument must be of type 'int'")
+    if n < 3:
+        return []
+    number = [False] + [True] * (n // 2 - 1)
+    for i in range(3, int(n**0.5 + 1), 2):
+        if number[(i - 1) // 2]:
+            number[i**2 // 2::i] = [False] * ((n - 1) // i - (n - 1) // (2 * i) - (i - 1) // 2)
+    return [2] + [2 * i + 1 for i in range(n // 2) if number[i]]
+
+
 count = 0
-for i in range(1000000):
-    if primality(i):
-        word = str(i)
-        d = deque(list(word))
-        flag = True
-        for j in range(len(word)):
-            d.rotate(1)
-            if not primality(int(''.join(d))):
-                flag = False
-                break
-        if flag:
-            count += 1
+N = 1000000
+for i in prime_sieve(N):
+    word = str(i)
+    d = deque(list(word))
+    flag = True
+    for j in range(len(word)):
+        d.rotate(1)
+        if not primality(int(''.join(d))):
+            flag = False
+            break
+    if flag:
+        count += 1
 print(count)
