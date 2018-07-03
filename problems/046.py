9774a98823bc19ac279a7018f7a398a99e5d04e8
Nahian Siddique <nsid10@hotmail.com>
2018-07-03 10:18:44 +0000

    Update 046.py

--- problems/046.py
+++ problems/046.py
@@ -1,14 +1,12 @@
 def primality(n):
     if type(n) != int:
         raise TypeError("argument must be of type 'int'")
-    if n < 2:
-        return False
     if n == 2:
         return True
-    if n % 2 == 0:
+    if n < 2 or n % 2 == 0:
         return False
-    for x in range(3, int(n**0.5 + 1), 2):
-        if n % x == 0:
+    for i in range(3, int(n**0.5 + 1), 2):
+        if n % i == 0:
             return False
     return True
 
