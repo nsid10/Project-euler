731ddddfe9c92e8836c2b520cc2837c45050915b
Nahian Siddique <nsid10@hotmail.com>
2018-07-05 08:14:48 +0000

    Update 076.py

--- problems/076.py
+++ problems/076.py
@@ -1,9 +1,5 @@
-num = 100
-ways = []
-for i in range(num + 1):
-    ways.append(0)
-ways[0] = 1
-for i in range(1, num + 1):
-    for j in range(i, num + 1):
+ways = [1] + [0] * 100
+for i in range(1, 101):
+    for j in range(i, 101):
         ways[j] += ways[j - i]
 print(ways[-1] - 1)
