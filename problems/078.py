107dc6def4547a0b8dc9d4bd4b6c7f0ee3d54b97
Nahian Siddique <nsid10@hotmail.com>
2018-07-05 11:34:33 +0000

    Update 078.py

--- problems/078.py
+++ problems/078.py
@@ -1,6 +1,6 @@
 from itertools import cycle
 
-penta = sum([[i * (3 * i - 1) // 2, i * (3 * i - 1) // 2 + i] for i in range(1, 250)], [])
+penta = sum([[i * (3 * i - 1) // 2, i * (3 * i - 1) // 2 + i] for i in range(1, 200)], [])
 p = [1] + [0] * 10**6
 for n in range(1, 10**6 + 1):
     sign = cycle([1, 1, -1, -1])
