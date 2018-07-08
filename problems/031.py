925ed21026d482738e7b3b41a077b420cc623ab3
Nahian Siddique <nsid10@hotmail.com>
2018-07-08 05:48:33 +0000

    Update 031.py

--- problems/031.py
+++ problems/031.py
@@ -1,9 +1,6 @@
 target = 200
 coinSizes = [1, 2, 5, 10, 20, 50, 100, 200]
-ways = [0]
-for i in range(200):
-    ways += [0]
-ways[0] = 1
+ways = [1] + [0] * target
 for i in range(len(coinSizes)):
     for j in range(coinSizes[i], target + 1):
         ways[j] += ways[j - coinSizes[i]]
