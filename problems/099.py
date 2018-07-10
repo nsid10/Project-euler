27a53af28ce5a34ecdcb150babd68ca66db2ec3b
Nahian Siddique <nsid10@hotmail.com>
2018-07-10 10:41:16 +0000

    Update 099.py

--- problems/099.py
+++ problems/099.py
@@ -1,12 +1,9 @@
 from math import log
 
-box = open("D:\Documents\Programming\Python\project euler\p099_base_exp.txt", "r")
-pos = 1
-line = 1
-large = 0
+box = open("/home/nahian/Documents/Python/p099_base_exp.txt", "r")
+pos, line, large = 1, 1, 0
 for i in box:
-    row = i.split()
-    row = list(map(int, row[0].split(',')))
+    row = list(map(int, i.split()[0].split(',')))
     if log(row[0]) * row[1] > large:
         large = log(row[0]) * row[1]
         line = pos
