ef8bae5400001973c2d0757b55c137ddba86b0c5
Nahian Siddique <nsid10@hotmail.com>
2018-07-11 06:08:21 +0000

    Update 100.py

--- problems/100.py
+++ problems/100.py
@@ -1,8 +1,4 @@
-b = 15
-n = 21
-while(n < 10**12):
-    btemp = 3 * b + 2 * n - 2
-    ntemp = 4 * b + 3 * n - 3
-    b = btemp
-    n = ntemp
+b, n = 15, 21
+while n < 10**12:
+    b, n = 3 * b + 2 * n - 2, 4 * b + 3 * n - 3
 print(b)
