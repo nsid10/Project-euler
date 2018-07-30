f8ec64cebe4d6161ac14bcd31e749e2f5bb29f28
Nahian Siddique <nsid10@hotmail.com>
2018-07-30 09:20:20 +0000

    Update 033.py

--- problems/033.py
+++ problems/033.py
@@ -1,28 +1,7 @@
-def curio(num, den):
-    n = str(num)
-    d = str(den)
-    if int(d[1]) != 0:
-        if n[1] == d[0] and (int(n[0]) / int(d[1])) == num / den:
-            return True
-    return False
-
-
-def fraction(num, den):
-    x = 2
-    while x <= num:
-        if num % x == 0 and den % x == 0:
-            num /= x
-            den /= x
-            continue
-        x += 1
-    return int(num), int(den)
-
-
-num = 1
-den = 1
-for i in range(10, 100):
-    for j in range(10, i):
-        if curio(j, i):
-            num *= j
-            den *= i
-print(fraction(num, den))
+d = 1
+for i in range(10):
+    for j in range(1, i):
+        q, r = divmod(9 * i * j, 10 * j - i)
+        if r == 0 and q < 10:
+            d *= i / j
+print(int(d))
